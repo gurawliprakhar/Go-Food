f827a8a4d1350af3b429aaacb777739bebbfda28
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2024-05-11 16:40:15 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import "./App.css";
-import Home from "./screens/Home";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./screens/login";
- // Corrected import statement
-import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
-import '../node_modules/bootstrap/dist/js/bootstrap.min'
+import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css"; // Corrected import statement
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"; // Added ".js" extension
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js"; // Added ".js" extension
+import Home from "./screens/Home";
+import Login from "./screens/login.js"; // Corrected import statement
 import Signup from "./screens/Signup";
+import { CartProvider } from "./components/ContextReducer";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} /> {/* Corrected element */}
-          <Route exact path="/createuser" element={<Signup />} />
-        </Routes>
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />{" "}
+            <Route exact path="/createuser" element={<Signup />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatchCart } from "./ContextReducer"; // Importing useDispatchCart
 
-function Card(props) {
-  const { foodName, img } = props; // Destructure props to access foodName and img
-  const options = props.options[0]; // Access the options object from the array
+function Card({ foodName, img, options }) {
+  const dispatch = useDispatchCart();
+  const [selectedSize, setSelectedSize] = useState(""); // State to manage the selected size
+
+  const handleAddToCart = async () => {
+    // Ensure a size is selected before adding to cart
+    if (!selectedSize) {
+      alert("Please select a size");
+      return;
+    }
+
+    // Dispatch action to add item to cart with selected size
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: { foodName, img, size: selectedSize },
+    });
+  };
+
+  const selectedOptions = options && options.length > 0 ? options[0] : {};
 
   return (
     <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
@@ -10,20 +27,13 @@ function Card(props) {
         src={img}
         className="card-img-top"
         alt="No Image"
-        style={{height:"120px", objectFit:"fit"}}
+        style={{ height: "120px", objectFit: "cover" }}
       />
       <div className="card-body">
-        <h5 className="card-title">{foodName}</h5> {/* Display foodName */}
+        <h5 className="card-title">{foodName}</h5>
         <div className="container w-100">
-          <select className="m-2 h-100 bg-success rounded">
-            {Array.from(Array(6), (_, i) => (
-              <option key={i + 1} value={i + 1}>
-                {i + 1}
-              </option>
-            ))}
-          </select>
-          <select className="m-2 h-100 bg-success rounded">
-            {Object.keys(options).map((size) => (
+          <select className="m-2 h-100 bg-success rounded" onChange={(e) => setSelectedSize(e.target.value)}>
+            {Object.keys(selectedOptions).map((size) => (
               <option key={size} value={size}>
                 {size}
               </option>
@@ -32,6 +42,13 @@ function Card(props) {
           <div className="d-inline h-100 fs-5">Total Price</div>
         </div>
       </div>
+      <hr />
+      <button
+        className="btn btn-success justify-center ms-2"
+        onClick={handleAddToCart}
+      >
+        Add to Cart
+      </button>
     </div>
   );
 }
